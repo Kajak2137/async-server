bcf2efec8a8b945267215733b12f9dcea5e1104d
Kajak2137 <kajos200@gmail.com>
2018-02-15 02:11:47 +0100

    Merge client and Server, add some things

--- kek.py
+++ kek.py
@@ -1,42 +1,65 @@
-
-import asyncore, socket
-from time import ctime
+import asyncore, socket, webbrowser
+from time import ctime, sleep
 from random import randint
 host = ''
-port = 997
+port = 1488
 clients = []
-link = raw_input("Link?: ")
 
 class Server(asyncore.dispatcher):
     def __init__(self, host, port):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.bind(('', port))
-        self.listen(1)
+        self.set_reuse_addr()
         print  "Listening on port", port
+        self.bind(('', port))
+        self.listen(5)
 
     def handle_accept(self):
         # when we get a client connection start a dispatcher for that
         # client
         socket, address = self.accept()
-        nickname = self.recv(1024)
-        print 'Connection by', nickname
+        print 'Connection by', address
         EchoHandler(socket)
-
 
 class EchoHandler(asyncore.dispatcher_with_send):
     # dispatcher_with_send extends the basic dispatcher to have an output
     # buffer that it writes whenever there's content
-    def handle_read(self):
 
-        print "going to socket"
-        print nickname
+    def handle_read(self):
+        link = raw_input('Enter Link: ')
         self.out_buffer = link
-        print "sent"
+        print self.out_buffer
         if not self.out_buffer:
             self.close()
 
-s = Server(host, port)
 
-asyncore.loop(timeout=1, count=10)
+class Client(asyncore.dispatcher_with_send):
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect((host, port))
+        self.out_buffer = ''
+
+    def handle_close(self):
+        self.close()
+        print 'Going Offline'
+
+    def handle_read(self):
+        while 1:
+            try: 
+                data = self.recv(1024)
+            except socket.error:
+                    if str(socket.error) == "[Errno 35] Resource temporarily unavailable":
+                        time.sleep(0)
+                        continue
+    
+    def handle_open(self):
+        print data
+        webbrowser.open(data, new=2);
+    
+                
+
+s = Server(host, port)
+c = Client(host, port)
+asyncore.loop()
 
